253c9db526a9c06c96ee3dc42acce574942e72a8
benchan777 <benchan777@gmail.com>
2021-01-26 19:46:37 -0800

    added print_songs method

--- Playlist.py
+++ Playlist.py
@@ -15,7 +15,7 @@ class Playlist:
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    song_counter = 1
+    song_counter = 0
     current_song = self.__first_song
 
     while current_song.get_title() != title:
@@ -23,9 +23,9 @@ class Playlist:
       current_song = current_song.get_next_song()
 
     if current_song.get_title() == title:
-      return f"{current_song.get_title()} is song #{song_counter} in the playlist."
+      return song_counter
     else:
-      return f"Song not found."
+      return -1
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
@@ -54,12 +54,13 @@ class Playlist:
   # 3. Song Title 3
 
   def print_songs(self):
-    pass
+    song_counter = 1
+    current_song = self.__first_song
 
-playlist = Playlist()
-playlist.add_song("cosmica")
-playlist.add_song("insomnia")
-playlist.add_song("djhflaja")
-playlist.add_song("messiah")
-print(playlist.length())
-print(playlist.find_song("djhflaja"))
+    while current_song.get_title() != None:
+      print(f"{song_counter}. {current_song.get_title()}")
+      song_counter += 1
+      current_song = current_song.get_next_song()
+
+      if current_song == None:
+        break
