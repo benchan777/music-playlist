32636b6e50ea2c7a2b4776ba35ba56d836e0085b
benchan777 <benchan777@gmail.com>
2021-01-26 23:47:53 -0800

    added remove_song method

--- Playlist.py
+++ Playlist.py
@@ -30,9 +30,23 @@ class Playlist:
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    pass
+    previous_song = self.__first_song
+    current_song = self.__first_song
 
+    if current_song.get_title() == title:
+      self.__first_song = self.__first_song.get_next_song()
 
+    while current_song.get_title() != title:
+      previous_song = current_song
+      current_song = current_song.get_next_song()
+
+      if current_song == None:
+        return f"Song not found."
+
+    if current_song.get_title() == title:
+      next_song = current_song.get_next_song()
+      previous_song.set_next_song(next_song)
+      return f"Deleted {current_song.get_title()} from the playlist."
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
